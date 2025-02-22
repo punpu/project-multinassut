a1dfc6332485d49c85c1ff0fd84f74297dfad0ac
Tuomas Kunnamo <t.a.kunnamo@gmail.com>
2025-02-22 17:15:00 +0200

    Lantern and morso updates

--- Assets/Scripts/MorsoBehavior.cs
+++ Assets/Scripts/MorsoBehavior.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
 using FishNet.Object;
 using FishNet.Connection;
-using FishNet.Managing.Logging;
-using FishNet.Object;
-using FishNet.Object.Prediction;
 using FishNet.Object.Synchronizing;
-using FishNet.Serializing.Helping;
-using FishNet.Transporting;
-using UnityEngine.InputSystem;
 
+// Note: Morso is a Player with this script and no Lantern!
 public class MorsoBehavior : NetworkBehaviour
 {
     private const float MIN_OPACITY = 0f;
@@ -17,12 +12,12 @@ public class MorsoBehavior : NetworkBehaviour
 
     private void Awake()
     {
-        _opacity.OnChange += on_opacity;
+        _opacity.OnChange += OnOpacity;
     }
 
     public override void OnOwnershipClient(NetworkConnection prevOwner)
     {
-        ModifyOpacity(0f); // Set Morso invisible by default
+        ModifyOpacity(-1f); // Set Morso invisible by default
     }
 
     [ServerRpc(RunLocally = true, RequireOwnership = false)]
@@ -30,12 +25,12 @@ public class MorsoBehavior : NetworkBehaviour
     {
         float prevValue = _opacity.Value;
         _opacity.Value = value;
-        on_opacity(prevValue, value, true); // Manually invoke the callback
+        OnOpacity(prevValue, value, true); // Manually invoke the callback
     }
 
     // SyncVar change event is only triggered when the value is changed on the server
     // and then synchronized to the clients.
-    private void on_opacity(float prev, float next, bool asServer)
+    private void OnOpacity(float prev, float next, bool asServer)
     {
         var renderer = GetComponentInChildren<MeshRenderer>();
         if (renderer != null)
@@ -45,11 +40,15 @@ public class MorsoBehavior : NetworkBehaviour
             renderer.material.color = color;
             renderer.enabled = next > 0; // Disable renderer so that we hide all visual effects
         }
+        else
+        {
+            Debug.LogWarning("Mesh renderer missing!");
+        }
     }
 
-    public void ModifyOpacity(float change)
+    public void ModifyOpacity(float deltaOpacity)
     {
-        SetOpacityServerRpc(Mathf.Clamp(_opacity.Value + change, MIN_OPACITY, MAX_OPACITY));
+        SetOpacityServerRpc(Mathf.Clamp(_opacity.Value + deltaOpacity, MIN_OPACITY, MAX_OPACITY));
     }
 
     public void ToggleOpacity()
@@ -57,36 +56,11 @@ public class MorsoBehavior : NetworkBehaviour
         SetOpacityServerRpc(_opacity.Value == MIN_OPACITY ? MAX_OPACITY : MIN_OPACITY);
     }
 
-    // Testing how to call other object's method
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        if (!IsOwner || context.phase != InputActionPhase.Started) { return; }
-
-        GameObject otherObject = GameObject.Find("Player(Clone)"); // TODO: figure out a nicer way to find the player
-        if (otherObject != null)
-        {
-            MorsoBehavior otherMorsoBehavior = otherObject.GetComponent<MorsoBehavior>();
-            if (otherMorsoBehavior != null)
-            {
-                otherMorsoBehavior.ModifyOpacity(0.2f);
-            }
-            else
-            {
-                Debug.LogWarning($"MorsoBehavior not found.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"GameObject not found.");
-        }
-    }
-
     void OnTriggerEnter (Collider other)
     {
         // Check if light is hitting Morso
         if (other.gameObject.CompareTag("Light"))
         {
-            Debug.Log($"Morso trigger, isLight");
             ModifyOpacity(1f);
         }
     }
@@ -96,7 +70,6 @@ public class MorsoBehavior : NetworkBehaviour
         // Check if light is leaving Morso
         if (other.gameObject.CompareTag("Light"))
         {
-            Debug.Log($"Morso trigger, isLight");
             ModifyOpacity(-1f);
         }
     }
