d2d2a5a875a3179dc89814ff5063750c61da28de
Minna Jarsma <minna.jarsma@gmail.com>
2025-02-24 18:04:52 +0200

    Use just AmmoPickup instead of AmmoSpawner

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -7,6 +7,40 @@ using FishNet.Object.Synchronizing;
 public class AmmoPickup : NetworkBehaviour
 {
     public float rotationSpeed = 20f;
+    public int respawnTimeInSeconds = 2;
+    private readonly SyncVar<bool> _isEnabled = new SyncVar<bool>(new SyncTypeSettings(WritePermission.ClientUnsynchronized, ReadPermission.ExcludeOwner));
+
+    private void Awake()
+    {
+        _isEnabled.OnChange += OnIsEnabled;
+    }
+
+    public override void OnOwnershipClient(NetworkConnection prevOwner)
+    {
+        EnableAmmoPickup();
+    }
+
+    // SyncVar change event is only triggered when the value is changed on the server
+    // and then synchronized to the clients.
+    private void OnIsEnabled(bool prev, bool next, bool asServer)
+    {
+        var boxCollider = gameObject.GetComponent<BoxCollider>();
+        var meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        if (!boxCollider || !meshRenderer)
+        {
+            Debug.LogWarning("AmmoPickup: BoxCollider or MeshRenderer missing!");
+            return;
+        }
+        gameObject.GetComponent<BoxCollider>().enabled = next;
+        gameObject.GetComponent<MeshRenderer>().enabled = next;
+    }
+
+    [ServerRpc(RunLocally = true, RequireOwnership = false)]
+    private void SetIsEnabledServerRpc(bool value)
+    {
+        var prevValue = _isEnabled.Value;
+        _isEnabled.Value = value;
+    }
 
     public void Update()
     {
@@ -14,14 +48,21 @@ public class AmmoPickup : NetworkBehaviour
     }
 
     [ServerRpc(RequireOwnership = false)]
+    private void DisableAmmoPickup()
+    {
+        SetIsEnabledServerRpc(false);
+        Invoke("EnableAmmoPickup", respawnTimeInSeconds);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void EnableAmmoPickup()
+    {
+        SetIsEnabledServerRpc(true);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
     public void OnPickUp()
     {
-        var parent = transform.parent.gameObject;
-        if (!parent)
-        {
-            Debug.LogWarning("AmmoPickup parent not found!");
-            return;
-        }
-        parent.GetComponent<AmmoSpawner>().DespawnAmmoPickup(gameObject);
+        DisableAmmoPickup();
     }
 }
